b5c265fd44417fb7b0a211fdcb49837c94d32749
BuuHQ <buuhq@tpb.com.vn>
2024-11-19 18:38:07 +0700

    20241119 cm5

--- blog-admin-ui/src/app/views/auth/login/login.component.ts
+++ blog-admin-ui/src/app/views/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AdminApiAuthApiClient } from 'src/app/api/admin-api.service.generated';
+import { AlertService } from 'src/app/shared/services/alert.service';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +11,17 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authApiClient: AdminApiAuthApiClient,
+    private alertService: AlertService,
+    private router: Router) {
+      this.loginForm = this.fb.group({
+        userName: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required),
+      });
+   }
 
 }
